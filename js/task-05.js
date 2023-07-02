bd528d606b780be0445857365f328159b79b0c20
Mia4678 <solomia.lutska@gmail.com>
2023-07-02 13:20:25 +0200

    new

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,9 @@ const textInput = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (event) => {
-  output.textContent = event.currentTarget.value;
+  if (textInput.value === "") {
+    output.textContent = "Anonymus";
+  } else {
+    output.textContent = event.currentTarget.value;
+  }
 });
